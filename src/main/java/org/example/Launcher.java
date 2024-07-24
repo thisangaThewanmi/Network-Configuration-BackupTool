39a8869c9e90742adfa8051751297bad66c70bf0
thisangaThewanmi <thisangat2005@gmail.com>
2024-07-24 20:39:54 +0530

    Created Launcher and LauncherWrapper

---
+++ src/main/java/org/example/Launcher.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent node = FXMLLoader.load(getClass().getResource("/veiw/Page1.fxml"));
+
+        Scene scene = new Scene(node);
+        stage.setScene(scene);
+        stage.setTitle("advsabj");
+        stage.centerOnScreen();
+        stage.show();
+
+    }
+}
